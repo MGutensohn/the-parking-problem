ac234fefaeb9457d0de8d81641201a4706aa892c
cernyc <ccerny@rollins.edugit config --glocal user.name cernycgit config --global user.email ccerny@rollins.edu>
2017-03-04 02:04:41 +0000

    working again

--- CarDetectorAlpha/piCarDetection.py
+++ CarDetectorAlpha/piCarDetection.py
@@ -33,7 +33,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     key = cv2.waitKey(1) & 0xFF
 
     # clear the stream in preparation for the next frame
-    rawCapture.truncate()
+    rawCapture.truncate(0)
 
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
