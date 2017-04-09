404e2086d1a90699700d534f5fb3c29cf66af3e0
chakib cerny <ccerny@chakibs-MacBook-Pro.local>
2017-04-09 12:38:20 -0400

    updateionic

--- parking_project/src/pages/hello-ionic/hello-ionic.ts
+++ parking_project/src/pages/hello-ionic/hello-ionic.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-
+import { NavController } from 'ionic-angular';
+import {Http} from '@angular/http';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-
-	
-  constructor() {
-
+  data:any
+  constructor(public navCtrl: NavController,http:Http) {
+     var response = http.get("http://localhost:3000/")
+     .map((res) =>res.json())
+     .subscribe((data)=>{
+     this.data = JSON.stringify(data);
+     console.log(response)
+   })
   }
 }
