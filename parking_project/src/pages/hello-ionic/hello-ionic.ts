e6ae4a3212b7e4cb5b28abdbc3ce787c8f106144
chakib cerny <ccerny@chakibs-MacBook-Pro.local>
2017-04-09 11:55:37 -0400

    update ionic2 code

--- parking_project/src/pages/hello-ionic/hello-ionic.ts
+++ parking_project/src/pages/hello-ionic/hello-ionic.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import {Http} from '@angular/http';
-import 'rxjs/Rx';
+
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  data:any
-  constructor(public navCtrl: NavController,http:Http) {
-     var response = http.get("http://localhost:3000/")
-     .map((res) =>res.json())
-     .subscribe((data)=>{
-     this.data = JSON.stringify(data);
-     console.log(response)
-   })
+
+	
+  constructor() {
+
   }
 }
